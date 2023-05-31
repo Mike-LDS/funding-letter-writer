da5c9dc5d1c45e961a72c70e12d07cf8b564285c
Mike-LDS <81445082+Mike-LDS@users.noreply.github.com>
2023-05-31 14:32:01 -0700

    Teachworks Update

--- fundingletterwriter.py
+++ fundingletterwriter.py
@@ -1,9 +1,21 @@
-import csv
 import docx
 from docxtpl import DocxTemplate
 import datetime
 import PySimpleGUI as sg
 import os
+import requests
+import json
+import sys
+import time
+
+url = "https://api.teachworks.com/v1/students"
+
+payload={}
+
+headers = {
+  'Authorization': 'Token token=st_live_fvXWm27jS_Q_BRtwYWH5lg',
+  'Content-Type': 'application/json'
+}
 
 content = {
     'first_name': 'Georgia',
@@ -18,20 +30,17 @@ content = {
 stu_first = []
 stu_last = []
 
-sg.theme("DarkBlue9")
+sg.theme("DarkTeal7")
 layout = [
     [sg.T("")],
     [sg.Text("First Name: "),sg.Input(key='FIRST',do_not_clear=False,size=(20, 20)),sg.Text("Last Name: "),sg.Input(key='LAST',do_not_clear=False,size=(20, 20)),sg.Button("Add Student")],
-    [sg.T("")],
-    [sg.Text("Choose Student Export: "), sg.Input(), sg.FileBrowse(file_types=(('CSV Files', '*.CSV'),), key='-USERS-')],
-    [sg.Text('Go to TC -> System -> Exports -> Click on Users -> Select Students from drop down menu -> Click Submit')],
     [sg.T("")],
     [sg.Text("Choose Letter Template:  "), sg.Input(), sg.FileBrowse(file_types=(('DOCX Files', '*.DOCX'),), key='-TEMP-')],
     [sg.T("")],
     [sg.Button("RUN"),sg.Text("", key='-TEXT-')]]
 
 # Building Window
-window = sg.Window('FUNDING LETTER WRITER', layout, size=(600,250))
+window = sg.Window('FUNDING LETTER WRITER', layout, size=(550,175))
 
 while True:
     event, values = window.read()
@@ -50,31 +59,39 @@ while True:
             sg.Popup('No Student First Name', title='ERROR')
         elif values['LAST'] == '' and len(stu_last) == 0:
             sg.Popup('No Student Last Name', title='ERROR')
+        elif values['-TEMP-'] == '':
+            sg.Popup('No Funding Template', title='ERROR')
         else:
             if values['FIRST'] != '' and values['LAST'] != '':
                 stu_first.append(values['FIRST'])
                 stu_last.append(values['LAST'])
             try:
-                with open(values['-USERS-'], newline='') as csvfile:
-                    reader = csv.reader(csvfile)
-                    letter_count = 0
-                    for row in reader:
-                        for x in range(len(stu_first)):
-                            if (row[4].lower() == stu_first[x].lower()) and (row[5].lower() == stu_last[x].lower()):
-                                content['first_name'] = row[4]
-                                content['last_name'] = row[5]
-                                content['ld_discription'] = row[27]
-                                content['program_objective'] = row[28]
-                                template = DocxTemplate(values['-TEMP-'])
-                                template.render(content)
-                                path = os.path.dirname(sys.executable).split('fundingletterwriter.app',-1)[0]
-                                template.save(path+'/Letters/'+content['first_name']+'_'+content['last_name']+'_'+content['year']+'_Funding_Quote.docx')
-                                letter_count = letter_count + 1
+                letter_count = 0
+                for x in range(0,len(stu_first)):
+                    stu_first[x] = stu_first[x].replace(' ', '+')
+                    stu_last[x] = stu_last[x].replace(' ', '+')
+                    stu_url = url+'?first_name='+stu_first[x]+'&last_name='+stu_last[x]
+                    response = requests.request("GET", stu_url, headers=headers, data=payload)
+                    student = response.json()
+                    content['first_name'] = student[0]["first_name"]
+                    content['last_name'] = student[0]["last_name"]
+                    cus = student[0]["custom_fields"]
+                    for i in range(0,len(cus)):
+                        if cus[i]['field_id'] == 15020:
+                            content['ld_discription'] = cus[i]['value']
+                        elif cus[i]['field_id'] == 15022:
+                            content['program_objective'] = cus[i]['value']
+                    template = DocxTemplate(values['-TEMP-'])
+                    template.render(content)
+                    path = os.path.dirname(sys.executable).split('fundingletterwriter.app',-1)[0]
+                    template.save(path+'/Letters/'+content['first_name']+'_'+content['last_name']+'_'+content['year']+'_Funding_Quote.docx')
+                    letter_count = letter_count + 1
+                    letters = len(stu_first)
+                    window['-TEXT-'].update(str(letter_count) +" OF "+ str(letters)+" LETTERS GENERATED")
+                    time.sleep(0.5)
 
-                letters = len(stu_first)
                 stu_first = []
                 stu_last = []
-                window['-TEXT-'].update(str(letter_count) +" OF "+ str(letters)+" LETTERS GENERATED")
             except Exception as e:
                 sg.Popup(e, title='ERROR')
 
